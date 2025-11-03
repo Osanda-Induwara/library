57e72bc94c94cee638c4b72a6819ff4856b17386
Osanda-Induwara <osanda2002@gmail.com>
2025-11-03 11:24:55 +0530

    renamed

--- src/main/java/com/example/library/repository/bookRepository.java
+++ src/main/java/com/example/library/repository/bookRepository.java
@@ -1,18 +1,9 @@
 package com.example.library.repository;
 
-import java.util.List;
-
+import com.example.library.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.library.model.Book;
-
 @Repository
-public interface bookRepository extends MongoRepository<Book, String> {
-
-    // Find all books published in the given year
-    List<Book> findByPublicationYear(int publicationYear);
-
-    // Delete all books published in the given year and return number deleted
-    long deleteByPublicationYear(int publicationYear);
+public interface BookRepository extends MongoRepository<Book, String> {
 }
